b81edb351f8093288648d7eb51ee84949385f7a1
Rafi Khaled <rkhaled@vassar.edu>
2020-06-03 02:10:51 -0400

    google style guide

--- library/src/main/java/MathUtils.java
+++ library/src/main/java/MathUtils.java
@@ -2,23 +2,23 @@ import java.util.List;
 
 public class MathUtils {
 
-    public int add(int a, int b){
-        return a+b;
-    }
+  public int add(int a, int b) {
+    return a + b;
+  }
 
-    public int sumOfArray(List<Integer> numbers){
-        int sum = 0;
-        for (Integer number : numbers){
-            sum += number;
-        }
-        return sum;
+  public int sumOfArray(List<Integer> numbers) {
+    int sum = 0;
+    for (Integer number : numbers) {
+      sum += number;
     }
+    return sum;
+  }
 
-    public double divide(int a, int b){
-        return a/b;
-    }
+  public double divide(int a, int b) {
+    return a / b;
+  }
 
-    public int computeArea(int r){
-        return (int)(Math.PI * Math.pow(r,2));
-    }
+  public int computeArea(int r) {
+    return (int) (Math.PI * Math.pow(r, 2));
+  }
 }
--- library/src/test/java/MathUtilsTest.java
+++ library/src/test/java/MathUtilsTest.java
@@ -7,61 +7,55 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
-    MathUtils mathUtils;
+  MathUtils mathUtils;
 
-    @BeforeEach
-    void init(){
-        mathUtils = new MathUtils();
-    }
+  @BeforeEach
+  void init() {
+    mathUtils = new MathUtils();
+  }
 
-    @Nested
-    class AddTest {
-        @Test
-        void testAddingTwoPositives() {
-            assertEquals(5, mathUtils.add(3, 2),
-                    "Add method should return the sum of two numbers");
-        }
-
-        @Test
-        void testAddingTwoNegatives() {
-            assertEquals(-14, mathUtils.add(-13, -1),
-                    "Add method should return the sum of two numbers");
-        }
-
-        @Test
-        void testAddingAPositiveAndANegative() {
-            assertEquals(0, mathUtils.add(-5, 5),
-                    "Add method should return the sum of two numbers");
-        }
+  @Nested
+  class AddTest {
+    @Test
+    void testAddingTwoPositives() {
+      assertEquals(5, mathUtils.add(3, 2), "Add method should return the sum of two numbers");
     }
 
     @Test
-    void testSumOfArray(){
-        assertAll(
-                () -> assertEquals(0, mathUtils.sumOfArray(Arrays.asList(new Integer[]{1, -1}))),
-                () -> assertEquals(6, mathUtils.sumOfArray(Arrays.asList(new Integer[]{1, 2, 3}))),
-                () -> assertEquals(0, mathUtils.sumOfArray(Arrays.asList(new Integer[]{})))
-        );
+    void testAddingTwoNegatives() {
+      assertEquals(-14, mathUtils.add(-13, -1), "Add method should return the sum of two numbers");
     }
-
-    @Nested
-    class DivideTest {
-        @Test
-        void testDivide(){
-            assertEquals(3,mathUtils.divide(6,2),
-                    "Divide method should divide two numbers");
-        }
-
-        @Test
-        void testDivideThrowsOnDivideByZero(){
-            assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0),
-                    "Divide by zero should throw");
-        }
-    }
-
 
     @Test
-    void testComputeArea() {
-        assertEquals(78,mathUtils.computeArea(5),"Compute area method should return circle area");
+    void testAddingAPositiveAndANegative() {
+      assertEquals(0, mathUtils.add(-5, 5), "Add method should return the sum of two numbers");
     }
+  }
+
+  @Test
+  void testSumOfArray() {
+    assertAll(
+        () -> assertEquals(0, mathUtils.sumOfArray(Arrays.asList(new Integer[] {1, -1}))),
+        () -> assertEquals(6, mathUtils.sumOfArray(Arrays.asList(new Integer[] {1, 2, 3}))),
+        () -> assertEquals(0, mathUtils.sumOfArray(Arrays.asList(new Integer[] {}))));
+  }
+
+  @Nested
+  class DivideTest {
+    @Test
+    void testDivide() {
+      assertEquals(3, mathUtils.divide(6, 2), "Divide method should divide two numbers");
+    }
+
+    @Test
+    void testDivideThrowsOnDivideByZero() {
+      assertThrows(
+          ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw");
+    }
+  }
+
+  @Test
+  void testComputeArea() {
+    assertEquals(78, mathUtils.computeArea(5), "Compute area method should return circle area");
+  }
 }
