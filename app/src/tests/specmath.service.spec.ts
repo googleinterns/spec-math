1d2508bc886f95f3a30ccb66be273f3356466e7f
Omar <omar.garciaf247@gmail.com>
2020-06-18 14:47:02 -0400

    adding tests

---
+++ app/src/tests/specmath.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { SpecMathService } from '../shared/services/specmath.service';
+
+describe('Call the Spec Math service', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [SpecMathService],
+    });
+  });
+
+  it('should respond with a conflict free response', () => {
+    
+  });
+});
