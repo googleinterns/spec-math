bb8c83def81e06e362d4fe6072ffcc6bdbb42e89
Omar <omar.garciaf247@gmail.com>
2020-06-24 00:42:34 -0400

    setting up mock objects for tests

--- app/src/tests/specmath.service.spec.ts
+++ app/src/tests/specmath.service.spec.ts
@@ -1,14 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SpecMathService } from '../shared/services/specmath.service';
 
-describe('Call the Spec Math service', () => {
+describe('SpecMathService', () => {
+  let httpMockObject: HttpClientTestingModule;
+  let service: SpecMathService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [SpecMathService],
     });
+    httpMockObject = TestBed.get(HttpClientTestingModule);
+    service = TestBed.get(SpecMathService);
   });
 
-  it('should respond with a conflict free response', () => {
-    console.log("I was called");
+  it('should be instantiated', () => {
+    expect(service).toBeTruthy();
   });
 });
