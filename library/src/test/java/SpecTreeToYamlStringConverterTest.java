39756952b7f061771cb0928ce21328747217b2bc
Rafi Khaled <rkhaled@vassar.edu>
2020-07-09 15:53:35 -0400

    license and change type

--- library/src/test/java/SpecTreeToYamlStringConverterTest.java
+++ library/src/test/java/SpecTreeToYamlStringConverterTest.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -9,7 +25,7 @@ import org.junit.jupiter.api.Test;
 
 class SpecTreeToYamlStringConverterTest {
   @Test
-  void testConvertMapToYamlString() throws IOException, UnexpectedDataException {
+  void testConvertMapToYamlString() throws IOException, UnexpectedTypeException {
     LinkedHashMap<String, Object> map1 =
         YamlStringToSpecTreeConverter.convertYamlFileToSpecTree(
             "src/test/resources/serializationSample.yaml");
@@ -29,7 +45,7 @@ class SpecTreeToYamlStringConverterTest {
     var specTreeToYamlStringsConverter = new SpecTreeToYamlStringConverter();
 
     assertThrows(
-        UnexpectedDataException.class,
+        UnexpectedTypeException.class,
         () -> specTreeToYamlStringsConverter.convertSpecTreeToYamlString(map1));
   }
 }
