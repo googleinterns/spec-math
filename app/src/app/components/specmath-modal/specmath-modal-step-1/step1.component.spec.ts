256e2bd8465a136d81f2de134ec54bdd24d69c56
Omar <omar.garciaf247@gmail.com>
2020-07-14 19:18:58 -0400

    Added tests for step 1

--- app/src/app/components/specmath-modal/specmath-modal-step-1/step1.component.spec.ts
+++ app/src/app/components/specmath-modal/specmath-modal-step-1/step1.component.spec.ts
@@ -12,4 +12,67 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { Step1Component } from './step1.component';
 
+import { MatDialogModule } from '@angular/material/dialog';
+import { By, BrowserModule } from '@angular/platform-browser';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const queryElement = (targetFixture: ComponentFixture<Step1Component>, targetClass: string) => {
+  return targetFixture.debugElement.query(By.css(targetClass));
+};
+
+describe('Step1Component', () => {
+  let fixture: ComponentFixture<Step1Component>;
+  let component: Step1Component;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        Step1Component
+      ],
+      imports: [
+        MatStepperModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatIconModule,
+        MatInputModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatTooltipModule,
+        BrowserModule,
+        MatDialogModule,
+        BrowserAnimationsModule
+      ],
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(Step1Component);
+      component = fixture.componentInstance;
+    });
+  }));
+
+  it('creates the Step1 component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('file name form validity', () => {
+    fixture.detectChanges();
+    const specNameField = component.specNameFormControl;
+    expect(specNameField.valid).toBeFalsy();
+
+    specNameField.setValue('invalid input!');
+    expect(specNameField.hasError('pattern')).toBeTruthy();
+
+    specNameField.setValue('');
+    expect(specNameField.hasError('required')).toBeTruthy();
+
+    specNameField.setValue('new_spec');
+    expect(specNameField.valid).toBeTruthy();
+  });
+});
