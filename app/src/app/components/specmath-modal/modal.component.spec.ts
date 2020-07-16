d460e4e910c2071d556e47a4f9755a33a1f78c51
Omar <omar.garciaf247@gmail.com>
2020-07-16 17:14:20 -0400

    removed unused modules from modal spec file

--- app/src/app/components/specmath-modal/modal.component.spec.ts
+++ app/src/app/components/specmath-modal/modal.component.spec.ts
@@ -21,7 +21,6 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { queryElement } from '../../../shared/functions';
@@ -44,8 +43,6 @@ describe('ModalComponent', () => {
         MatDialogModule,
         MatIconModule,
         MatInputModule,
-        FormsModule,
-        ReactiveFormsModule,
         MatTooltipModule,
         BrowserModule,
         MatDialogModule,
