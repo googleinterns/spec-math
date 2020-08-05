65e452d2ab037721138ec72c3d6c505e7538d9bd
Omar <omar.garciaf247@gmail.com>
2020-08-05 14:17:14 -0400

    working on render spec files

--- app/src/app/components/spec-math-display-results/display-results.component.ts
+++ app/src/app/components/spec-math-display-results/display-results.component.ts
@@ -12,21 +12,79 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Component, Input, AfterViewInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { OperationSet } from 'src/shared/interfaces';
 import * as yaml from 'js-yaml';
 import { readFileAsString } from 'src/shared/functions';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-display-results',
   templateUrl: './display-results.component.html',
   styleUrls: ['./display-results.component.scss']
 })
-export class DisplayResultsComponent implements AfterViewInit {
+export class DisplayResultsComponent implements OnInit {
   @Input() operationSet: OperationSet;
+  resultsRendered = false;
+  defaultsRendered = false;
+  specsRendered = new Array(this.operationSet.specFiles.length).fill(false);
 
   downloadOperationSet() {
     console.log('create and download zip');
+  }
+
+  specLabel(index: number) {
+    return `Spec ${index + 1}`;
+  }
+
+  renderYamlToDiv(divId: string, yamlJSON: object) {
+    const renderDiv = document.getElementById(divId);
+    Object.keys(yamlJSON).forEach((key) => {
+      const fileTextNode = document.createTextNode(`${key}: ${yamlJSON[key]}`);
+      const renderNode = document.createElement('p').appendChild(fileTextNode);
+      renderDiv.appendChild(renderNode);
+      renderDiv.appendChild(document.createElement('br'));
+    });
+  }
+
+  renderResultsFile() {
+    if (this.resultsRendered) {
+      return;
+    }
+    this.resultsRendered = true;
+
+    readFileAsString(this.operationSet.resultSpec.file).then((res) => {
+      this.renderYamlToDiv('results-file-render', yaml.load(res));
+    });
+  }
+
+  renderDefaultsFile() {
+    if (this.defaultsRendered) {
+      return;
+    }
+    this.defaultsRendered = true;
+
+    readFileAsString(this.operationSet.defaultsFile).then((res) => {
+      this.renderYamlToDiv('defaults-file-render', yaml.load(res));
+    });
+  }
+
+  renderFile(event: MatTabChangeEvent) {
+    const tab = event.tab.textLabel;
+
+    switch (tab) {
+      case 'Defaults':
+        this.renderDefaultsFile();
+        break;
+      case 'Result':
+        this.renderResultsFile();
+        break;
+    }
+
+    if (tab.includes('Spec')) {
+      const index = parseInt(tab.slice(tab.length - 1), 10) - 1;
+      console.log(index);
+    }
   }
 
   get defaultsFileValid(): boolean {
@@ -49,36 +107,13 @@ export class DisplayResultsComponent implements AfterViewInit {
     return this.operationSet.specFiles;
   }
 
-  renderYamlToDiv(divId: string, yamlJSON: object) {
-    const renderDiv = document.getElementById(divId);
-
-    console.log(renderDiv);
-
-    Object.keys(yamlJSON).forEach((key) => {
-      const fileTextNode = document.createTextNode(`${key}: ${yamlJSON[key]}`);
-      const renderNode = document.createElement('p').appendChild(fileTextNode);
-      renderDiv.appendChild(renderNode);
-      renderDiv.appendChild(document.createElement('br'));
-    });
-  }
-
-  ngAfterViewInit() {
-    readFileAsString(this.operationSet.resultSpec.file).then((res) => {
-      this.renderYamlToDiv('results-file-render',  yaml.load(res));
-    });
-
-    if (this.defaultsFileValid) {
-      readFileAsString(this.operationSet.defaultsFile).then((res) => {
-        console.log(yaml.load(res));
-        this.renderYamlToDiv('defaults-file-render',  yaml.load(res));
-      });
-    }
-
-    this.operationSet.specFiles.forEach((file, index) => {
-      readFileAsString(file).then((res) => {
-        console.log(yaml.load(res));
-        this.renderYamlToDiv(`spec-file-${index}-render`,  yaml.load(res));
-      });
-    });
+  ngOnInit() {
+    this.renderResultsFile();
+    // this.operationSet.specFiles.forEach((file, index) => {
+    //   readFileAsString(file).then((res) => {
+    //     console.log(yaml.load(res));
+    //     this.renderYamlToDiv(`spec-file-${index}-render`, yaml.load(res));
+    //   });
+    // });
   }
 }
