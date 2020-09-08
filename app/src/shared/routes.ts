cc8477721d4795aa45f527098247ba610ccdbdd4
Omar <omar.garciaf247@gmail.com>
2020-09-08 15:53:55 -0400

    fixed urls

--- app/src/shared/routes.ts
+++ app/src/shared/routes.ts
@@ -1,6 +1,6 @@
 export const SPEC_MATH_URL = 'https://specmath-dot-cloud-ace-dev.uc.r.appspot.com';
 export const routes = {
-  mergeSpecs: 'operations/merge',
+  mergeSpecs: '/operations/merge',
   test: 'http://localhost:3000/merge',
   home: '/',
   results: 'results',
